3ff0ac4f238d90834dc9b847de6b23134a38a4f9
codewerfer <29753931+codewerfer@users.noreply.github.com>
2019-03-26 16:45:31 +0100

    working on

--- Ex01/Segment.h
+++ Ex01/Segment.h
@@ -21,12 +21,15 @@ public:
 
   public:
     Point() {}
+
     Point(double x, double y) : x(x), y(y) {}
 
     double getX();
+
     double setX(double x);
 
     double getY();
+
     double setY(double y);
   };
 
@@ -40,21 +43,59 @@ public:
   // returns Euclidean length of vector
   double norm();
 
+  // returns signed distance d of a point to this segment
+  // if (near) 0, we are on the line.
+  double distance(Point p);
+
+  // returns intersect of this segment with other
+  bool intersect(Segment other);
+
+  // returns intersect of this segment with other, and where it happens
+  bool intersect(Segment other, Point &where);
+
 private:
   Point start;
   Point end;
+
+  double det();
+
+  double det(Point a, Point b);
 };
 
 double Segment::Point::getX() { return x; }
 
-double Segment::Point::setX(double x) { return this->x = x;}
+double Segment::Point::setX(double x) { return this->x = x; }
 
 double Segment::Point::getY() { return y; }
 
-double Segment::Point::setY(double y) { return this->y = y;}
+double Segment::Point::setY(double y) { return this->y = y; }
 
 double Segment::norm()
 {
   return sqrt(pow(end.getX() - start.getX(), 2) +
               pow(end.getY() - start.getY(), 2));
+}
+
+double Segment::distance(Segment::Point p)
+{
+  // d = (x-x1)(y2-y1)-(y-y1)(x2-x1)
+  // dnorm = d/norm()
+  return ((p.getX() - start.getX()) * (end.getY() - start.getY()) -
+          (p.getY() - start.getY()) * (end.getX() - start.getX()))
+         / norm();
+}
+
+double Segment::det(Segment::Point a, Segment::Point b)
+{
+  return a.getX() * b.getY() - a.getY() * b.getX();
+}
+
+double Segment::det()
+{
+  return det(start, end);
+}
+
+bool Segment::intersect()
+{
+
 }
